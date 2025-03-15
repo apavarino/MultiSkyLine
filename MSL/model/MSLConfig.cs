3e435e8b9d837019bf2e863b0954bee9586f8240
ldemoulins <louis.demoulins@sfr.fr>
2025-03-15 09:19:10 -0400

    Removed debug logs

--- MSL/model/MSLConfig.cs
+++ MSL/model/MSLConfig.cs
@@ -6,35 +6,7 @@ namespace MSL.model
     [Serializable]
     public class MSLConfig
     {
-        private string _serverURL = "127.0.0.1";
-        
-        public string ServerURL {
-            get
-            {
-                MslLogger.LogWarn("Getting ServerURL");
-                return _serverURL;
-            }
-            set
-            {
-                MslLogger.LogWarn("Setting ServerUrl to " + value);
-                _serverURL = value;
-            }
-        }
-        
-        private bool _isServerEnabled = true;
-
-        public bool IsServerEnabled
-        {
-            get
-            {
-                MslLogger.LogWarn("Getting IsServerEnabled");
-                return _isServerEnabled;
-            }
-            set
-            {
-                MslLogger.LogWarn("Setting IsServerEnabled to " + value);
-                _isServerEnabled = value;
-            }
-        }
+        public string ServerURL { get; set; } = "127.0.0.1";
+        public bool IsServerEnabled { get; set; } = true;
     }
 }
